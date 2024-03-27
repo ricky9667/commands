2ae7def09ea8d7865a14b68dd34001b7358592cc
Ricky Hu <ricky9667@gmail.com>
2024-03-27 11:20:18 +0800

    Add some general commands

--- general.zsh
+++ general.zsh
@@ -1,8 +1,24 @@
 alias o="open"
-alias c="code"
-alias rmf="rm -rf"
-alias py="python3"
-alias pip="pip3"
+alias sz="source ~/.zshrc"
+
+# eza
 alias l="eza --icons -l -a"
 
+# zoxide
+alias cd="z"
+eval "$(zoxide init zsh)"
+
+# bat
+alias cat="bat"
 export BAT_THEME="TwoDark"
+
+# VS Code
+alias c="code"
+
+# Python
+alias py="python3"
+alias pip="pip3"
+
+# Fuck
+eval $(thefuck --alias)
+
