9f250d1befba0d4a3ab59c8e85357c4b8722097d
Ricky Hu <ricky9667@gmail.com>
2025-06-03 17:33:42 +0800

    Add 1 commit git reset

--- git.zsh
+++ git.zsh
@@ -3,4 +3,6 @@ alias gc="git commit -m"
 alias gx="git clean -fd"
 alias g="gitui" # Should install https://github.com/extrawurst/gitui
 alias gcz="git cz" # Should install https://github.com/streamich/git-cz
+alias grs1="git reset HEAD^"
+
 
